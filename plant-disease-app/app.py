f5ccf608dbc26cabbe6aa91d46fbfeac1149441c
kotagiri kubhushan <118363478+kotagiri-kulbhushan@users.noreply.github.com>
2026-03-03 02:05:07 +0530

    Update app.py

--- plant-disease-app/app.py
+++ plant-disease-app/app.py
@@ -130,5 +130,6 @@ def uploaded_file(filename):
 # Run App
 # ======================
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
